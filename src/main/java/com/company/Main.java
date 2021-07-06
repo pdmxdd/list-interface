d67a0462ef89289635fabbbeb19bd22bcd24cab7
pdmxdd <pdmxdd@gmail.com>
2021-07-06 10:08:27 -0500

    list-interface

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // command click or control click ArrayList to take a look at what it implements
+        ArrayList<String> stringList = new ArrayList<>();
+        // go further into the List interface it implements
+        // notice this is where it is defined that the ArrayList must implement multiple methods
+    }
+}
